eb23c18e56a355633ffa80ca88f7bad60bdc818f
Hossein HassanNejad <hosseinspell@gmail.com>
2021-02-14 22:55:41 +0330

    intent with transation

--- app/src/main/java/com/example/loginui/LoginActivity.kt
+++ app/src/main/java/com/example/loginui/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.loginui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registBtn = findViewById<Button>(R.id.btn_regist_in_login)
+
+        registBtn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left)
+        }
+
     }
 }
--- app/src/main/java/com/example/loginui/RegisterActivity.kt
+++ app/src/main/java/com/example/loginui/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.example.loginui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val loginBtn = findViewById<Button>(R.id.btn_login_in_regist)
+
+        loginBtn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right)
+        }
     }
 }
